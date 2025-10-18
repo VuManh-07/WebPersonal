049a7de6943004b56073f8397f5b4566eabfdae4
ManhVu-07 <manhvd1507@gmail.com>
2025-10-18 15:30:02 +0700

    feature: add new script for menu

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navMenu = document.querySelector('nav ul');
+
+  menuToggle.addEventListener('click', function () {
+    navMenu.classList.toggle('active');
+    menuToggle.querySelector('i').classList.toggle('fa-bars')
+    menuToggle.querySelector('i').classList.toggle('fa-times')
+  })
+
+  document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function () {
+      navMenu.classList.remove('active');
+      menuToggle.querySelector('i').classList.add('fa-bars')
+      menuToggle.querySelector('i').classList.remove('fa-times')
+    })
+  })
+
   // ====================================
   // === [1] HIỆU ỨNG SKILL PROGRESS ===
   // ====================================
